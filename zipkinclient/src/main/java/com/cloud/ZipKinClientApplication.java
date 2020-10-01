fa5188ea7fd9c35e3ff198c3b202ed612ac50ce1
github-lvpf <lpf18366313221@outlook.com>
2020-10-02 00:15:47 +0800

    message

--- zipkinclient/src/main/java/com/cloud/ZipKinClientApplication.java
+++ zipkinclient/src/main/java/com/cloud/ZipKinClientApplication.java
@@ -1,4 +1,4 @@
-package java.cloud;
+package com.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- zipkinclient/src/main/java/com/cloud/controller/ZipKinController.java
+++ zipkinclient/src/main/java/com/cloud/controller/ZipKinController.java
@@ -1,4 +1,4 @@
-package java.cloud.controller;
+package com.cloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
